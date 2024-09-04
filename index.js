b9bdad7709e77c63c60e218fcf62d23ed547437d
koleso8 <dev.koleso@gmail.com>
2024-09-04 18:05:51 +0300

    add animation

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 import Game from './src/game.js';
 import View from './src/view.js';
 
-const root = document.querySelector('#root');
+const root = document.querySelector('.root');
 
 const game = new Game();
-const view = new View(root, 480, 640, 20, 10);
+const view = new View(root, 320, 640, 20, 10);
 
 window.game = game;
 window.view = view;
 
-console.log(game);
+document.addEventListener('keydown', event => {
+  switch (event.code) {
+    //!'ArrowLeft'
+    case 'ArrowLeft' || 'KeyA': //
+      game.movePieceLeft();
+      view.render(game.getState());
+      break;
+    //!ArrowUp'
+    case 'ArrowUp' || 'KeyW':
+      game.rotatePiece();
+      view.render(game.getState());
+      break;
+    //!'ArrowRight'
+    case 'ArrowRight' || 'KeyD':
+      game.movePieceRight();
+      view.render(game.getState());
+      break;
+    //!'ArrowDown'
+    case 'ArrowDown' || 'KeyS':
+      game.movePieceDown();
+      view.render(game.getState());
+      break;
+  }
+});
--- src/game.js
+++ src/game.js
@@ -2,43 +2,12 @@ export default class Game {
   score = 0;
   lines = 0;
   level = 0;
-  playField = [
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-  ];
+  playField = this.createPlayField();
   activePieceX = 0;
   activePieceY = 0;
-  activePiece = {
-    x: 0,
-    y: 0,
-    get blocks() {
-      return this.rotations[this.rotionIndex];
-    },
-    blocks: [
-      [0, 1, 0],
-      [1, 1, 1],
-      [0, 0, 0],
-    ],
+  activePiece = this.createPiece();
 
-    /*  rotionIndex: 0,
+  /*  rotionIndex: 0,
     rotations: [
       [
         [0, 1, 0],
@@ -61,7 +30,111 @@ export default class Game {
         [0, 1, 0],
       ],
     ], */
-  };
+
+  nextPiece = this.createPiece();
+
+  getState() {
+    const playField = this.createPlayField();
+    const { y: pieceY, x: pieceX, blocks } = this.activePiece;
+
+    for (let y = 0; y < this.playField.length; y++) {
+      playField[y] = [];
+
+      for (let x = 0; x < this.playField[y].length; x++) {
+        playField[y][x] = this.playField[y][x];
+      }
+    }
+
+    for (let y = 0; y < blocks.length; y++) {
+      for (let x = 0; x < blocks[y].length; x++) {
+        if (blocks[y][x]) {
+          playField[pieceY + y][pieceX + x] = blocks[y][x];
+        }
+      }
+    }
+
+    return { playField };
+  }
+  createPlayField() {
+    const playField = [];
+
+    for (let y = 0; y < 20; y++) {
+      playField[y] = [];
+
+      for (let x = 0; x < 10; x++) {
+        playField[y][x] = 0;
+      }
+    }
+    return playField;
+  }
+
+  createPiece() {
+    const index = Math.floor(Math.random() * 7);
+
+    const type = 'IJLOSTZ'[index];
+    const piece = {};
+
+    switch (type) {
+      case 'I':
+        piece.blocks = [
+          [0, 0, 0, 0],
+          [1, 1, 1, 1],
+          [0, 0, 0, 0],
+          [0, 0, 0, 0],
+        ];
+        break;
+      case 'J':
+        piece.blocks = [
+          [0, 0, 0],
+          [2, 2, 2],
+          [0, 0, 2],
+        ];
+        break;
+      case 'L':
+        piece.blocks = [
+          [0, 0, 0],
+          [3, 3, 3],
+          [3, 0, 0],
+        ];
+        break;
+      case 'O':
+        piece.blocks = [
+          [0, 0, 0, 0],
+          [0, 4, 4, 0],
+          [0, 4, 4, 0],
+          [0, 0, 0, 0],
+        ];
+        break;
+      case 'S':
+        piece.blocks = [
+          [0, 0, 0],
+          [0, 5, 5],
+          [5, 5, 0],
+        ];
+        break;
+      case 'T':
+        piece.blocks = [
+          [0, 0, 0],
+          [6, 6, 6],
+          [0, 6, 0],
+        ];
+        break;
+      case 'Z':
+        piece.blocks = [
+          [0, 0, 0],
+          [7, 7, 0],
+          [0, 7, 7],
+        ];
+        break;
+      default:
+        throw new Error('Невідома фігура');
+    }
+
+    piece.x = Math.floor((10 - piece.blocks[0].length) / 2);
+    piece.y = -1;
+
+    return piece;
+  }
 
   movePieceLeft() {
     this.activePiece.x -= 1;
@@ -85,6 +158,8 @@ export default class Game {
     if (this.hasCollision()) {
       this.activePiece.y -= 1;
       this.lockPiece();
+      this.clearLines();
+      this.updatePieces();
     }
   }
 
@@ -160,5 +235,35 @@ export default class Game {
         }
       }
     }
+  }
+
+  clearLines() {
+    const rows = 20;
+    const columns = 10;
+    let lines = [];
+
+    for (let y = rows - 1; y >= 0; y--) {
+      let numberOfBlocks = 0;
+
+      for (let x = 0; x < columns; x++) {
+        if (this.playField[y][x]) {
+          numberOfBlocks += 1;
+        }
+      }
+
+      if (numberOfBlocks === 0) {
+        break;
+      } else if (numberOfBlocks < columns) {
+        continue;
+      } else if (numberOfBlocks === columns) {
+        lines.unshift(y);
+      }
+    }
+    console.log(lines);
+  }
+
+  updatePieces() {
+    this.activePiece = this.nextPiece;
+    this.nextPiece = this.createPiece();
   }
 }
